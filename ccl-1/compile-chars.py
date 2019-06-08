3858f031f09f7250ede02577155ddac6d21157fa
alex <alex@virtualbox>
2019-06-08 17:32:19 -0400

    add MLP chinese book 2 and 3 practice

--- ccl-1/compile-chars.py
+++ ccl-1/compile-chars.py
@@ -2,18 +2,34 @@ import sys
 import os
 import subprocess
 
-chars = []
-for line in open(sys.argv[1], 'r').readlines():
-    for w in line.strip().split():
-        if w in chars: continue
-        chars.append(w)
 target='char-practice-body.tex'
 if os.path.exists(target):
     os.rename(target, target +".orig")
 
-with open(target, 'w') as f:
-    for w in chars:
-        # f.write("\\miline{%s}\n" % w)
-        f.write("\\milinelvb{%s}\n" % w)
+f = open(target, 'w')
 
-subprocess.check_call("xelatex char-practice.tex", shell=True)
+iline, chars = 0, []
+for line in open(sys.argv[1], 'r').readlines():
+    line = line.strip()
+    if not line: continue
+    for i,w in enumerate(line.split()):
+        if w in chars: continue
+        if i == 0:
+            # if iline > 0:
+            #     f.write("\\clearpage\n\\newpage\n")
+            f.write("\n\n{\\Large %s}\n\n" % w)
+        else:
+            # f.write("\\miline{%s}\n" % w)
+            f.write("\\milinelvb{%s}\n" % w)
+    iline += 1
+f.close()
+
+
+for i in range(2):
+    subprocess.check_call("xelatex char-practice.tex", shell=True)
+
+basename, ext = os.path.splitext(sys.argv[1])
+newpdf = "{}.pdf".format(basename)
+if os.path.exists(newpdf):
+    os.rename(newpdf, "{}.orig.pdf".format(basename))
+os.rename("char-practice.pdf", newpdf)
